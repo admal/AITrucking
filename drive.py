72eb3b1d28f8eac2656629f1ee96543bca866271
admal <adam.malewski.wp.pl>
2018-01-07 21:04:18 +0100

    Example using inputs with gamepad

--- drive.py
+++ drive.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import inputs
 
 from Utils.grabscreen import *
 from Utils.directkeys import *
@@ -8,6 +9,14 @@ from config import *
 
 
 def start():
+	print('3 sec to start')
+	time.sleep(3)
+
+	while True:
+		events = inputs.get_gamepad()
+		for event in events:
+			print(event.ev_type, event.code, event.state)
+
 	while True:
 		frame_started = time.time()
 		screen = grab_screen(region=(0, 40, SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,7 +25,8 @@ def start():
 		cv2.imshow('AITrucking', screen)
 
 		print('frame took: {}s'.format(time.time() - frame_started))
-		
+		PressKey(W)
+
 		if cv2.waitKey(25) & 0xFF == ord('q'):
 			cv2.destroyAllWindows()
 			break
