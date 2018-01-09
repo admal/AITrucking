ce4a7429a52238aa98140ccea8e06f9dc0a52a73
admal <adam.malewski.wp.pl>
2018-01-09 20:48:17 +0100

    Generating training data

--- Controls/InputControl.py
+++ Controls/InputControl.py
@@ -1,35 +1,16 @@
-# class InputHandler:
-# 	_key_codes_handlers = None
-
-# 	def _init_key_codes():
-# 		pass
-
-# 	def hanle_event(input_event):
-# 		pass
-
-# class GamePadTrainingInputHandler(InputHandler):
-# 	_max_analog_val = 32767
-
-# 	def _init_key_codes():
-# 		_key_codes_handlers = {
-# 			'ABS_RZ': handle_right_trigger,
-# 			'ABS_Z': handle_left_trigger,
-# 			'ABS_X': handle_left_analog
-# 		}
-
-# 	def handle_event(input_event, callback):
-		
-#ABS_RZ - right trigger
-#ABS_Z - left trigger
-#ABS_X - left analog
 import time
+from datetime import datetime
+import cv2
+import numpy as np
+
+from config import *
 
 class GamepadInputTrainingHandler:
-	_file = None
+	writer = None
 	_last_time_handle = None
 	_frequency = 0.1
-	def __init__(self, file, frequency = 10):
-		self._file = file
+	def __init__(self, writer, frequency = 10):
+		self.writer = writer
 		self._last_time_handle = time.time()
 		self._frequency = 1 / frequency
 
@@ -37,11 +18,18 @@ class GamepadInputTrainingHandler:
 	def handle_input(self, screen, input_controller):
 		if (time.time() - self._last_time_handle) >= self._frequency:
 			pad_input = input_controller.get_input()
-
-			#TODO: what to do with screen
-			#screenshot, rotation, throttle, brake
-			data_row = [screen, pad_input['A_X'], pad_input['RT'], pad_input['LT']]
-
+			dst = process_screen(screen)
+			if SHOW_FRAMES:
+				cv2.imshow("win1", dst)
+			
+			img_filename = TRAINING_DIRECTORY + '\\Images\\' + datetime.now().strftime('%d%m%Y%H%M%S%f') + '.jpg'
+			cv2.imwrite(img_filename, dst)
+			self.writer.writerow([img_filename, pad_input['A_X'], pad_input['RT'], pad_input['LT']])
 			# print('rotation', 'throttle', 'brake')
 			print(pad_input['A_X'], pad_input['RT'], pad_input['LT'])
 			self._last_time_handle = time.time()
+
+
+def process_screen(screen):
+	dst = cv2.resize(screen, (IMAGE_WIDTH, IMAGE_HEIGHT))
+	return dst
--- drive.py
+++ drive.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import csv
 
 from Utils.grabscreen import *
 from Utils.directkeys import *
@@ -14,18 +15,20 @@ def start():
 
 	input_controller = GamepadInputController()
 	input_controller.init()
+	training_data_file  = open(TRAINING_DIRECTORY + '\\train_data.csv', "w")
+	writer = csv.writer(training_data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-	handler = GamepadInputTrainingHandler(None)
+	handler = GamepadInputTrainingHandler(writer)
 	while True:
 		frame_started = time.time()
 		screen = grab_screen(region=(0, 40, SCREEN_WIDTH, SCREEN_HEIGHT))
+		handler.handle_input(screen, input_controller)
+		
+		if cv2.waitKey(25) & 0xFF == ord('q'):
+			cv2.destroyAllWindows()
+			break
 
-		handler.handle_input(None, input_controller)
-		# print('frame took: {}s'.format(time.time() - frame_started))
-		# if cv2.waitKey(25) & 0xFF == ord('q'):
-		# 	cv2.destroyAllWindows()
-		# 	break
-
+	training_data_file.close()
 
 
 if __name__ == '__main__':
